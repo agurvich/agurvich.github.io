31c93bac1aeac9420440d88ff5cc7555572db39e
Alex Gurvich <agurvich@u.northwestern.edu>
2022-09-15 22:14:08 -0500

    track new built files

--- docs/assets/img/make_mipmap.sh
+++ docs/assets/img/make_mipmap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 filename=`echo "${1%.*}"`
-echo $filename
-convert $1 -resize 50% -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB ${filename}_50.jpg
-convert $1 -resize 25% -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB ${filename}_25.jpg
-convert $1 -resize 12.5% -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB ${filename}_125.jpg
+full_output=bkg_${filename}.png
+width=1000
+height=195
+background=white
+background=transparent
+
+convert $1 -resize  ${width}x${height} -background ${background} -gravity north -extent ${width}x$((height+5)) ${full_output}
+convert ${full_output} -crop ${width}x${height} -background ${background} -flatten ${full_output}
+
+convert ${full_output} -resize 50% -sampling-factor 4:2:0  bkg_${filename}_50.png
+convert ${full_output} -resize 25% -sampling-factor 4:2:0 bkg_${filename}_25.png
+convert ${full_output} -resize 12.5% -sampling-factor 4:2:0 bkg_${filename}_125.png
