daf33dd509553be2c90942e570500e04059d13f5
Alex Gurvich <agurvich@u.northwestern.edu>
2023-04-13 19:49:31 -0500

    add node_blog prefix to assets

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = {
     // Set the build output directory to 'dist'
     images:{
         unoptimized:true
-    }
+    },
+    basePath: '/docs',
+    assetPrefix: '/docs/',
     // ... other configurations
   }
