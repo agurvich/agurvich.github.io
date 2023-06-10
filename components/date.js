d00bd333a793037229675225b411c6d53a0e94a8
Alex Gurvich <agurvich@u.northwestern.edu>
2023-06-10 11:46:52 -0400

    add ability to show only year

--- components/date.js
+++ components/date.js
@@ -2,5 +2,10 @@ import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  if (dateString.length === 4){
+    return <time dateTime={dateString}>{format(date, 'yyyy')}</time>;
+  }
+  else{
+    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  }
 }
