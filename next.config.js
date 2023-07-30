2f13fc9eaa6427ff739ab15aaf408821fbfbf012
Alex Gurvich <agurvich@u.northwestern.edu>
2023-07-30 16:51:04 -0400

    add files that make next fast?

---
+++ next.config.js
@@ -0,0 +1,12 @@
+
+// next.config.js
+
+module.exports = {
+    // Set the build output directory to 'dist'
+    images:{
+        unoptimized:true
+    },
+    basePath: '',
+    assetPrefix: '',
+    // ... other configurations
+  }
