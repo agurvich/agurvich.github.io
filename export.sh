89d806b6e06f2bbdf7b6f87ca9c68498a0087963
Alex Gurvich <agurvich@u.northwestern.edu>
2024-10-29 16:27:46 -0400

    replace public website, here we go

--- export.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp export-next.config.js next.config.js
-npx next build
-npx next export -o docs
-cp CNAME docs
-> docs/.nojekyll
--- public/images/make_mipmap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-convert $1 -resize 80% -sampling-factor 4:2:0  output.png
