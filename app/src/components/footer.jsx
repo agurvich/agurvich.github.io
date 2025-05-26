2c64ab509eb6e91bb4f574c8de76a2833c8614df
Alex Gurvich <agurvich@u.northwestern.edu>
2025-05-26 15:11:33 -0400

    automatically update copyright date

--- app/src/components/footer.jsx
+++ app/src/components/footer.jsx
@@ -11,10 +11,12 @@ export function scrollToElement(name) {
 }
 
 function Footer({...pars}){
+
+    const year = new Date().getFullYear();
     return (
         <nav className="flex justify-between items-center w-screen bg-gray-800 text-white p-4 pr-8 z-10">
             <div className="text-start">
-                <p>&copy; 2024 Alex Gurvich. All rights reserved.</p>
+                <p>&copy; {year} Alex Gurvich. All rights reserved.</p>
             </div>
             <div className="flex justify-center space-x-4">
                 <a href="https://www.linkedin.com/in/alex-b-gurvich" target="_blank" rel="noopener noreferrer">
